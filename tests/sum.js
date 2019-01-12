e9a116d284a037909e5aac4c71e8a8d67a59a013
luc <luctunechi45@gmail.com>
2019-01-12 11:28:15 +0200

    sum file modified

--- tests/sum.js
+++ tests/sum.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const { sum } = require('../src');
 
-describe('Sum of two integer', () => {
-  it('should add the sum of two integers', () => {
+describe('Sum of two integers', () => {
+  it('should add the sum of 6 integers', () => {
   expect(sum(1, 2 , 3 , 4 , 4 , 5)).to.equal(19);
  });
 });
